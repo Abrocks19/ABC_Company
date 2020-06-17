b067721a0e62b13bef7e21a3528b19a3d54d83b4
Abrocks19 <63560903+Abrocks19@users.noreply.github.com>
2020-06-17 11:09:29 +0530

    Add files via upload

---
+++ ExportImportController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ABC_Company.Models;
+
+namespace ABC_Company.Controllers
+{
+    public class ExportImportController : ApiController
+    {
+        // GET api/exportImport
+        public IEnumerable<usp_GetDataByCountry_Result> Get()
+        {
+            using (ABCCompanyEntities objABCCompanyEntities = new ABCCompanyEntities())
+            {
+                List<usp_GetDataByCountry_Result> listExportImportData = objABCCompanyEntities.usp_GetDataByCountry().ToList();
+                return listExportImportData;
+            }           
+        }        
+    }
+}
---
+++ TradesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ABC_Company.Models;
+
+namespace ABC_Company.Controllers
+{
+    public class TradesController : ApiController
+    {
+        private ABCCompanyEntities db = new ABCCompanyEntities();
+
+        // GET: api/Trades
+        public IQueryable<Trade> GetTrades()
+        {
+            return db.Trades;
+        }        
+
+        // POST: api/Trades
+        [ResponseType(typeof(Trade))]
+        public IHttpActionResult PostTrade(Trade trade)
+        {
+            db.Trades.Add(trade);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = trade.ID }, trade);
+        }       
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }        
+    }
+}
